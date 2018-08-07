0019390966c4b6e9dc45ffe2173f520bf64f74ef
Matthew Carter <m@ahungry.com>
2018-08-07 00:56:54 -0400

    Parser stuff

--- example/math.ts
+++ example/math.ts
@@ -1,3 +1,23 @@
-const add = (a: number, b: number): number => a + b
+export const add = (a: number, b: number): number => a + b
+export let add2 = (a: number, b: number): number => a + b
+export var add3 = (a: number, b: number): number => a + b
 
-console.log(add(1, 2))
+var z = 9
+
+function adder (a: number, b: number): number {
+  return add(a, b)
+}
+
+export class MyMath {
+  public foo = 3
+  public blub = (x: string): string => x
+
+  constructor (private a: number, private b: number) {
+  }
+
+  public add () {
+    return add(this.a, this.b)
+  }
+}
+
+// console.log(add(1, 2))
